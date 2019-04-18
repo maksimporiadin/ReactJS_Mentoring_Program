be83cf6c7c1bcd8b14e87d6f38c49adc4c86c8cc
Maksim_Poriadin <Maksim_Poriadin@epam.com>
2019-04-18 20:06:25 +0300

    HW-6-Routing:  Fix bug with title and genre.

--- src/components/FilterHeader/SearchParam/index.js
+++ src/components/FilterHeader/SearchParam/index.js
@@ -6,23 +6,21 @@ import './style.css';
 
 const SearchParam = ({ searchBy, onChange, children}) => (
     <div className="searchParam" onChange={onChange}>
-        <span className="searchParam-title">Search By</span>
-        <label htmlFor="search-title"
-               className={`btn ${searchBy === SEARCH_BY.TITLE.value? 'btn-primary' : 'btn-secondary'} btn-sm searchParam-input`}>
-            <input type="radio"
-                   name="search"
-                   id="search-title"
-                   value={SEARCH_BY.TITLE.value}
-                   defaultChecked={searchBy === SEARCH_BY.TITLE.value}/> {SEARCH_BY.TITLE.name}
-        </label>
-        <label htmlFor="search-genre"
-               className={`btn ${searchBy === SEARCH_BY.GENRE.value? 'btn-primary' : 'btn-secondary'} btn-sm searchParam-input`}>
-            <input type="radio"
-                   name="search"
-                   id="search-genre"
-                   value={SEARCH_BY.GENRE.value}
-                   defaultChecked={searchBy === SEARCH_BY.GENRE.value}/> {SEARCH_BY.GENRE.name}
-        </label>
+            <span className="searchParam-title">Search By</span>
+            <label htmlFor="search-title"
+                   className={`btn ${searchBy === SEARCH_BY.TITLE.value? 'btn-primary' : 'btn-secondary'} btn-sm searchParam-input`}>
+                <input type="radio"
+                       name="search"
+                       id="search-title"
+                       value={SEARCH_BY.TITLE.value}/> {SEARCH_BY.TITLE.name}
+            </label>
+            <label htmlFor="search-genre"
+                   className={`btn ${searchBy === SEARCH_BY.GENRE.value? 'btn-primary' : 'btn-secondary'} btn-sm searchParam-input`}>
+                <input type="radio"
+                       name="search"
+                       id="search-genre"
+                       value={SEARCH_BY.GENRE.value}/> {SEARCH_BY.GENRE.name}
+            </label>
         <div className="searchParam-grow"></div>
         {children}
     </div>
